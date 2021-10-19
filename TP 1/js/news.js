6dca4dfc924e9027f212f3d96f47b2f74ab3462f
Ignacio Rivas <nachoiar@gmail.com>
2021-10-19 18:37:48 -0300

    Agrego carpeta js y archivo en la misma

---
+++ TP 1/js/news.js
@@ -0,0 +1,8 @@
+"user strict";
+
+let btn = document.getElementById("btn-news");
+btn.addEventListener("click", showNews);
+
+function showNews(){
+    alert("Hola, este msj se genero con JavaScript");
+}
